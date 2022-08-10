032f0fc5484f6422cbea85cc853a07f0d93fcb20
Kolya Ryabtsev <kolecuk@mail.ru>
2022-08-10 15:54:39 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const str = Math.abs(n).toString();
+    const arr = str.split('');
+    const arr2 = arr.map((item, index, array) => {
+        return item = array[arr.length - 1 - index];
+    });
+    const str2 = arr2.join('');
+    const res = +str2;
+    return res;
 }
