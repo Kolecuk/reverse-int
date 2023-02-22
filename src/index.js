ed817263663c998056e5f12edd8df5a0ab3c5ce9
Kolya Ryabtsev <kolecuk@mail.ru>
2023-02-23 02:22:16 +0300

    feat: add another solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function reverse(n) {
     const str = Math.abs(n).toString();
     const arr = str.split('');
-    const arr2 = arr.map((item, index, array) => {
-        return item = array[arr.length - 1 - index];
-    });
-    const str2 = arr2.join('');
-    const res = +str2;
+    const arrReverse = arr.reverse()
+    const strReverse = arrReverse.join('');
+    const res = +strReverse;
     return res;
 }
